bb2021cf703dfa0224af7372e4eaf50c5e621610
Myron <dubtwee@gmail.com>
2021-01-16 12:11:26 -0600

    comments scores db

---
+++ database/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO "reviews" ("comments", "score")
+VALUES ('What an experience', 2),
+('It tastes like snoz berries', 4);
--- database/init.sql
+++ database/init.sql
@@ -7,3 +7,9 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "reviews" (
+  "id" SERIAL PRIMARY KEY,
+  "comments" VARCHAR (100),
+  "score" INT
+);
