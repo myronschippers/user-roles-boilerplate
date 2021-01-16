3bbba992fa869a22d6883952e9f0d55d82a95b01
Myron <dubtwee@gmail.com>
2021-01-16 12:18:57 -0600

    moving data insert for db roles to the data file

--- database/data.sql
+++ database/data.sql
@@ -1,3 +1,4 @@
-INSERT INTO "reviews" ("comments", "score")
-VALUES ('What an experience', 2),
-('It tastes like snoz berries', 4);
+INSERT INTO "roles" ("label", "access_level")
+VALUES ('admin', 0),
+('manager', 3),
+('peon', 6);
--- database/init.sql
+++ database/init.sql
@@ -1,15 +1,16 @@
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
-CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+CREATE TABLE "roles" (
+	"id" serial primary key,
+	"label" varchar(60) not null,
+	"access_level" int not null
 );
 
-CREATE TABLE "reviews" (
+CREATE TABLE "user" (
   "id" SERIAL PRIMARY KEY,
-  "comments" VARCHAR (100),
-  "score" INT
+  "username" VARCHAR (80) UNIQUE,
+  "password" VARCHAR (1000),
+  "email" VARCHAR(80) UNIQUE NOT NULL,
+  "first_name" VARCHAR(100) NOT NULL,
+  "last_name" VARCHAR(100) NOT NULL,
+  "temp_reg_id" VARCHAR(500),
+  "role_id" INT REFERENCES "roles"
 );
